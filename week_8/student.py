33532445ba17fe7787dc2f5aad10dad15986d998
Jarkthan <thanakorn.jark@gmail.com>
2023-10-18 08:35:38 +0700

    d

---
+++ week_8/student.py
@@ -0,0 +1,12 @@
+def main():
+    student = get_student()
+    print(f"{student[0]} from {student[1]}")
+
+def get_student():
+    student = {}
+    student["name"] = input("Name: ")
+    student["house"] = input("House: ")
+    return student
+
+if __name__ == "__main__":
+    main()
